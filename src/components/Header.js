cbf8c57a07a0dc973aae3fc8574518c3c1caa64c
JoaGoz <jorgealberto021203@gmail.com>
2025-05-26 19:22:15 -0600

    changes

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Header() {
+    return (
+        <header className="header">
+            <div className="logo">
+                <img src="/assets/images/logo.png"></img>
+            </div>
+            <nav>
+                <ul>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/stories">Historias</a></li>
+                    <li><a href="/aboutus">Sobre Nosotros</a></li>
+                    <li><a href="/comments">Comentarios</a></li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
