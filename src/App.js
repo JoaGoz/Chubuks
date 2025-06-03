94cc19bfffa00fe137a035f97434f624f8220c3b
JoaGoz <jorgealberto021203@gmail.com>
2025-06-03 14:46:44 -0600

    components

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import React from "react";
 import './App.css';
 import Header from "./components/Header"
 import PhraseBanner from "./components/PhraseBanner"
+import Welcome from "./components/Welcome"
 
 function App() {
   return (
     <>
       <Header />
       <PhraseBanner />
+      <Welcome />
     </>
   );
 }
